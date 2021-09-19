6aa70362f9b820ef48a3909bb364df2cf8515518
Vladimir9812 <fomin.v97@yandex.ru>
2021-09-19 14:25:10 +0300

    Updates

---
+++ precache-manifest.051eb394641904fa28d6e62e1b266f93.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b9e40ea2d27a766bd0b",
+    "url": "/Vladimir9812/movie-database/static/css/main.16be3cc2.chunk.css"
+  },
+  {
+    "revision": "3b9e40ea2d27a766bd0b",
+    "url": "/Vladimir9812/movie-database/static/js/main.cb4ab6d9.chunk.js"
+  },
+  {
+    "revision": "95445486b62e855c59a0",
+    "url": "/Vladimir9812/movie-database/static/js/runtime~main.9ffe2926.js"
+  },
+  {
+    "revision": "2cb4bcf9a91e6b2008f6",
+    "url": "/Vladimir9812/movie-database/static/css/2.8009e843.chunk.css"
+  },
+  {
+    "revision": "2cb4bcf9a91e6b2008f6",
+    "url": "/Vladimir9812/movie-database/static/js/2.60cf0d9e.chunk.js"
+  },
+  {
+    "revision": "5f280ca1fb86d02d68ced77276669bd0",
+    "url": "/Vladimir9812/movie-database/static/media/logo.5f280ca1.png"
+  },
+  {
+    "revision": "ad7a38f732051ca251311c822845ca91",
+    "url": "/Vladimir9812/movie-database/static/media/user.ad7a38f7.png"
+  },
+  {
+    "revision": "c687256ec30e9fa0e6c50ca81bb9d96a",
+    "url": "/Vladimir9812/movie-database/static/media/footer.c687256e.png"
+  },
+  {
+    "revision": "15f21fad2c4ff0488c67e203a792e9d6",
+    "url": "/Vladimir9812/movie-database/static/media/star.15f21fad.png"
+  },
+  {
+    "revision": "ff2e8b544b2f256b1f7274d4d793605f",
+    "url": "/Vladimir9812/movie-database/static/media/heart.ff2e8b54.png"
+  },
+  {
+    "revision": "228d0151eaef62eb7e994d9ab3dee6e7",
+    "url": "/Vladimir9812/movie-database/static/media/play.228d0151.png"
+  },
+  {
+    "revision": "a5046e7493cf0c539f28c6491cf112f4",
+    "url": "/Vladimir9812/movie-database/static/media/404.a5046e74.png"
+  },
+  {
+    "revision": "a6a0c970aef388e30b6dd0a49f05ff58",
+    "url": "/Vladimir9812/movie-database/static/media/advanture.a6a0c970.jpg"
+  },
+  {
+    "revision": "2c7188c752c2a54fb38ae73843405dbf",
+    "url": "/Vladimir9812/movie-database/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Vladimir9812/movie-database/precache-manifest.051eb394641904fa28d6e62e1b266f93.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Vladimir9812/movie-database/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
