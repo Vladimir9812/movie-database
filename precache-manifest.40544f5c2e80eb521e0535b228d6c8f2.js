a567ca08e6afdd69c58924d5d71dec87af08ac97
Vladimir9812 <fomin.v97@yandex.ru>
2021-09-19 14:39:39 +0300

    Updates

---
+++ precache-manifest.40544f5c2e80eb521e0535b228d6c8f2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "0153171cd0bd9f29d58d",
+    "url": "/Vladimir9812/movie-database/build/static/static/css/main.7a62b66d.chunk.css"
+  },
+  {
+    "revision": "0153171cd0bd9f29d58d",
+    "url": "/Vladimir9812/movie-database/build/static/static/js/main.4a81f3fc.chunk.js"
+  },
+  {
+    "revision": "9636b92ed9a31388c066",
+    "url": "/Vladimir9812/movie-database/build/static/static/js/runtime~main.515f465a.js"
+  },
+  {
+    "revision": "844250afb72bc7ef18d2",
+    "url": "/Vladimir9812/movie-database/build/static/static/css/2.8009e843.chunk.css"
+  },
+  {
+    "revision": "844250afb72bc7ef18d2",
+    "url": "/Vladimir9812/movie-database/build/static/static/js/2.55f9ea65.chunk.js"
+  },
+  {
+    "revision": "5f280ca1fb86d02d68ced77276669bd0",
+    "url": "/Vladimir9812/movie-database/build/static/static/media/logo.5f280ca1.png"
+  },
+  {
+    "revision": "ad7a38f732051ca251311c822845ca91",
+    "url": "/Vladimir9812/movie-database/build/static/static/media/user.ad7a38f7.png"
+  },
+  {
+    "revision": "c687256ec30e9fa0e6c50ca81bb9d96a",
+    "url": "/Vladimir9812/movie-database/build/static/static/media/footer.c687256e.png"
+  },
+  {
+    "revision": "15f21fad2c4ff0488c67e203a792e9d6",
+    "url": "/Vladimir9812/movie-database/build/static/static/media/star.15f21fad.png"
+  },
+  {
+    "revision": "ff2e8b544b2f256b1f7274d4d793605f",
+    "url": "/Vladimir9812/movie-database/build/static/static/media/heart.ff2e8b54.png"
+  },
+  {
+    "revision": "228d0151eaef62eb7e994d9ab3dee6e7",
+    "url": "/Vladimir9812/movie-database/build/static/static/media/play.228d0151.png"
+  },
+  {
+    "revision": "a5046e7493cf0c539f28c6491cf112f4",
+    "url": "/Vladimir9812/movie-database/build/static/static/media/404.a5046e74.png"
+  },
+  {
+    "revision": "a6a0c970aef388e30b6dd0a49f05ff58",
+    "url": "/Vladimir9812/movie-database/build/static/static/media/advanture.a6a0c970.jpg"
+  },
+  {
+    "revision": "d9d2de38b3d13e3404ec08c5747801b7",
+    "url": "/Vladimir9812/movie-database/build/static/index.html"
+  }
+];
