45551b173c9cfbf0cbe7ebaecb2f5a89357369c8
Vladimir9812 <fomin.v97@yandex.ru>
2021-09-19 14:43:12 +0300

    Updates

---
+++ precache-manifest.4ce65a7281bab4ca18e65a8388632a77.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec658be5ced7207c1473",
+    "url": "/Vladimir9812/movie-database/build/static/css/main.26d8e9a8.chunk.css"
+  },
+  {
+    "revision": "ec658be5ced7207c1473",
+    "url": "/Vladimir9812/movie-database/build/static/js/main.5d91c5f7.chunk.js"
+  },
+  {
+    "revision": "b2894cf7b5afaa498af1",
+    "url": "/Vladimir9812/movie-database/build/static/js/runtime~main.48e1211f.js"
+  },
+  {
+    "revision": "42f2a9af9a7336165348",
+    "url": "/Vladimir9812/movie-database/build/static/css/2.8009e843.chunk.css"
+  },
+  {
+    "revision": "42f2a9af9a7336165348",
+    "url": "/Vladimir9812/movie-database/build/static/js/2.1b08d910.chunk.js"
+  },
+  {
+    "revision": "5f280ca1fb86d02d68ced77276669bd0",
+    "url": "/Vladimir9812/movie-database/build/static/media/logo.5f280ca1.png"
+  },
+  {
+    "revision": "ad7a38f732051ca251311c822845ca91",
+    "url": "/Vladimir9812/movie-database/build/static/media/user.ad7a38f7.png"
+  },
+  {
+    "revision": "c687256ec30e9fa0e6c50ca81bb9d96a",
+    "url": "/Vladimir9812/movie-database/build/static/media/footer.c687256e.png"
+  },
+  {
+    "revision": "15f21fad2c4ff0488c67e203a792e9d6",
+    "url": "/Vladimir9812/movie-database/build/static/media/star.15f21fad.png"
+  },
+  {
+    "revision": "ff2e8b544b2f256b1f7274d4d793605f",
+    "url": "/Vladimir9812/movie-database/build/static/media/heart.ff2e8b54.png"
+  },
+  {
+    "revision": "228d0151eaef62eb7e994d9ab3dee6e7",
+    "url": "/Vladimir9812/movie-database/build/static/media/play.228d0151.png"
+  },
+  {
+    "revision": "a5046e7493cf0c539f28c6491cf112f4",
+    "url": "/Vladimir9812/movie-database/build/static/media/404.a5046e74.png"
+  },
+  {
+    "revision": "a6a0c970aef388e30b6dd0a49f05ff58",
+    "url": "/Vladimir9812/movie-database/build/static/media/advanture.a6a0c970.jpg"
+  },
+  {
+    "revision": "d22c92256bb2c2328fc165ca93036f0f",
+    "url": "/Vladimir9812/movie-database/build/index.html"
+  }
+];
